17b6f2c38a9283b6be9ac62aecce6967446a9fe0
GulshanKumar-cod <68959184+GulshanKumar-cod@users.noreply.github.com>
2022-07-09 18:48:38 +0530

    Commit

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="container">
+      <div className="card mt-4">
+        <h1 className="mx-3">About Notebook</h1>
+        <h4 className="card-body my-3">Notebook is an online notes storing webapp, wherein the user is able to create, read, update or delete their notes conveniently from their computer, or any suitable device by logging into their account. The application runs on the cloud services server and data is stored securely there.</h4>
+      </div>
+    </div>
+  );
+};
+
+export default About;
